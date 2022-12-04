67187d6ba628396eabff0fb6573747ed13160cf5
dioma <dioma>
2022-12-04 20:01:49 +0000

    Tweet object created

---
+++ src/User.py
@@ -0,0 +1,29 @@
+import json
+
+
+class Tweet:
+    def __init__(self, tweet_id, user_id, text, time):
+        self.tweet_id = tweet_id
+        self.user_id = user_id
+        self.text = text
+        self.time = time
+        
+    def __eq__(self, __o: object) -> bool:
+        return self.tweet_id == __o.tweet_id
+    
+    def to_json(self):
+        return json.dumps({
+            'tweet_id': self.tweet_id,
+            'user_id': self.user_id,
+            'text': self.text,
+            'time': self.time
+        })
+        
+    @staticmethod
+    def from_json(json_str):
+        if not json_str:
+            return None
+        json_obj = json.loads(json_str)
+        return Tweet(json_obj['tweet_id'], json_obj['user_id'], json_obj['text'], json_obj['time'])
+    
+        
