90bd68c52be79a8d74495eca617ea2ef250cd1fc
dioma <dioma>
2022-12-04 19:57:48 +0000

    File reestructure

--- src/Node.py
+++ src/Node.py
@@ -11,12 +11,16 @@ async def run(id):
     node = Server()
     await node.listen(NODE_PORT + id)
     await node.bootstrap([("0.0.0.0", 8468)])
-
+    
+        
     # set a value for the key "my-key" on the network
-    await node.set(str(id), "my awesome value is " + str(id))
+    await node.set(str(id) + "oalao", "my awesome value is " + str(id))
 
     # get the value associated with "my-key" from the network
-    result = await node.get(str(id))
+    result = await node.get(str(id) +  "oalao")
     print(result)
+        
+    while True:
+        await asyncio.sleep(1)
 
 #asyncio.run(run(1))
