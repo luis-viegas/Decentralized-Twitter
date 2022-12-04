abdb88f2507b9d1981e26e73dd127739de7cc350
luis-viegas <luiscarlosviegas@icloud.com>
2022-12-04 14:24:15 +0000

    initial setup suggestion

---
+++ Node.py
@@ -0,0 +1,22 @@
+import asyncio
+from kademlia.network import Server
+from config import NODE_PORT
+
+
+def start(id):
+    asyncio.run(run(id))
+
+async def run(id):
+    # Create a node and start listening on port 5678
+    node = Server()
+    await node.listen(NODE_PORT + id)
+    await node.bootstrap([("0.0.0.0", 8468)])
+
+    # set a value for the key "my-key" on the network
+    await node.set(str(id), "my awesome value is " + str(id))
+
+    # get the value associated with "my-key" from the network
+    result = await node.get(str(id))
+    print(result)
+
+#asyncio.run(run(1))
---
+++ Origin.py
@@ -0,0 +1,33 @@
+import asyncio
+from kademlia.network import Server
+from config import ORIGIN_PORT
+
+import logging
+import asyncio
+
+from kademlia.network import Server
+
+def run():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    log = logging.getLogger('kademlia')
+    log.addHandler(handler)
+    log.setLevel(logging.DEBUG)
+    loop.set_debug(True)
+
+    server = Server()
+    loop.run_until_complete(server.listen(ORIGIN_PORT))
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        server.stop()
+        loop.close()
+
